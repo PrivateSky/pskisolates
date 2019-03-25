f315b1008b83036004ccc65da69c860fa53d5aed
StefanCosminR <stefancosmin.romanescu@gmail.com>
2019-03-25 15:38:11 +0200

    Prepared main isolate class

--- IsolatedExecutionEnvironment.js
+++ IsolatedExecutionEnvironment.js
@@ -1,22 +1,78 @@
 let ivm = require('isolated-vm');
-const WebSocket = require('ws');
+const utils = require('./utils/utils');
+const defaultConfig = require('./IsolateConfig')();
+const debuggerServer = require('./debugger');
 
 function IsolatedExecutionEnvironment(config) {
-    const useInspector = config.useInspector;
-
-    const isolate = new ivm.Isolate({ inspector: useInspector });
-    const isolateGlobal = context.global;
-
-    if(useInspector) {
-        startDebugger(isolate);
+    /** INITIALIZING OBJECT STATE **/
+    if (!utils.hasIdenticalObjectKeysHierarchy(config, defaultConfig)) {
+        throw new Error('Invalid config argument received');
     }
 
+    const useInspector = config.debug.useInspector;
+
+    const isolate = new ivm.Isolate({inspector: useInspector});
+    const context = isolate.createContextSync({inspector: useInspector});
+    const isolateGlobal = context.global;
+
+    if (useInspector) {
+        debuggerServer.startDebugger(isolate, config.debug.debuggerPort);
+    }
+
+    /** PREPARING ENVIRONMENT STATE **/
     isolateGlobal.setSync('global', isolateGlobal.derefInto());
-    jail.setSync('_ivm', ivm);
-    jail.setSync('_console', deepReference(console, ivm));
+    isolateGlobal.setSync('_ivm', ivm);
+    isolateGlobal.setSync('_console', _deepReference(console, ivm));
+    isolateGlobal.setSync('_setTimeout', new ivm.Reference(function (timeout, callbackRef) {
+        setTimeout(function () {
+            callbackRef.applyIgnored(undefined, []);
+        }, timeout);
+    }));
+    isolateGlobal.setSync('_require', new ivm.Reference(function (name) {
+        return _deepReference(require(name), ivm);
+    }));
 
 
-    function deepReference(obj, ivm, depth = 0) {
+    /** PUBLIC METHODS **/
+    async function prepareGlobalEnv(code) {
+        await delay(config.debug.delay);
+
+        const prepareScript = await isolate.compileScript(code, {filename: 'prepareEnvironment.js'});
+        await prepareScript.run(context);
+
+        return this;
+    }
+
+
+    async function prepareRequire(code) {
+        await delay(config.debug.delay);
+
+        const prepareScript = await isolate.compileScript(code, {filename: 'prepareRequire.js'});
+        await prepareScript.run(context);
+
+        return this;
+    }
+
+    async function secureGlobalEnv(code) {
+        await delay(config.debug.delay);
+
+        const prepareScript = await isolate.compileScript(code, {filename: 'secureEnvironment.js'});
+        await prepareScript.run(context);
+
+        return this;
+    }
+
+    async function run(code) {
+        await delay(config.runtime.delay);
+
+        const prepareScript = await isolate.compileScript(code, {filename: 'isolateRuntime.js'});
+        await prepareScript.run(context);
+    }
+
+    /** INTERNAL METHODS **/
+
+
+    function _deepReference(obj, ivm, depth = 0) {
         let newObj = {};
 
         if (!ivm) {
@@ -27,7 +83,7 @@ function IsolatedExecutionEnvironment(config) {
             Object.keys(obj).forEach(key => {
                 // might not be good enough
                 if (typeof obj[key] === 'object' && !Buffer.isBuffer(key)) {
-                    newObj[key] = deepReference(obj[key], ivm, depth - 1);
+                    newObj[key] = _deepReference(obj[key], ivm, depth - 1);
                 }
             });
         } else {
@@ -36,50 +92,15 @@ function IsolatedExecutionEnvironment(config) {
 
         return newObj;
     }
+
+    const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+    /** EXPORTS **/
+
+    this.prepareGlobalEnv = prepareGlobalEnv;
+    this.prepareRequire = prepareRequire;
+    this.secureGlobalEnv = secureGlobalEnv;
 }
 
-function IsolateConfig() {
-    return {
-        useInspector: false
-    }
-}
-
-function startDebugger(isolate, port = 10000) {
-    let wss = new WebSocket.Server({ port: port });
-
-    wss.on('connection', function(ws) {
-        // Dispose inspector session on websocket disconnect
-        let channel = isolate.createInspectorSession();
-        function dispose() {
-            try {
-                channel.dispose();
-            } catch (err) {}
-        }
-        ws.on('error', dispose);
-        ws.on('close', dispose);
-
-        // Relay messages from frontend to backend
-        ws.on('message', function(message) {
-            try {
-                channel.dispatchProtocolMessage(message);
-            } catch (err) {
-                // This happens if inspector session was closed unexpectedly
-                ws.close();
-            }
-        });
-
-        // Relay messages from backend to frontend
-        function send(message) {
-            try {
-                ws.send(message);
-            } catch (err) {
-                dispose();
-            }
-        }
-        channel.onResponse = (callId, message) => send(message);
-        channel.onNotification = send;
-    });
-    console.log(`Inspector: chrome-devtools://devtools/bundled/inspector.html?experiments=true&v8only=true&ws=127.0.0.1:${port}`);
-}
 
 module.exports = IsolatedExecutionEnvironment;
